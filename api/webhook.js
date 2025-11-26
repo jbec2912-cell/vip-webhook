68bbc17537c1d26cd080d11012c564504c49dd91
jbec2912-cell <jbec2912@gmail.com>
2025-11-26 10:40:52 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,16 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    console.log("Incoming Twilio webhook:", req.body);
 
+    const twiml = `
+      <Response>
+        <Say>Hello. Your webhook is working.</Say>
+      </Response>
+    `;
+
+    res.setHeader("Content-Type", "text/xml");
+    return res.status(200).send(twiml);
+  }
+
+  return res.status(405).json({ message: "Method Not Allowed" });
+}
